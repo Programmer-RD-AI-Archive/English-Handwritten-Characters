6e63caf573fb549fb80144171e8e1075580dd376
Programmer-RD-AI <go2ranuga@gmail.com>
2022-04-28 21:53:48 +0530

    History
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ .history/Model/preproccessing_20220428205927.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import torch
+from torchvision.transforms import *
+
+# Transformations
+class PreProccessing
---
+++ .history/Model/preproccessing_20220428210005.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import torch
+from torchvision.transforms import *
+
+# Transformations
+class PreProccessing:
+    def __init__(self) -> None:
+        pass
+    
+    
