4346c7aa510bb84755d77387d7fe54b0a1199200
Programmer-RD-AI <go2ranuga@gmail.com>
2022-04-28 22:11:53 +0530

    Update
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ .history/Model/preproccessing_20220428221121.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np
+import torch
+from torchvision.transforms import *
+
+# Transformations
+class PreProccessing:
+    def __init__(self,random_vertical_flip:bool=True,color_jitter:bool=True,random_grayscale:bool=True,random_horizontal_flip:bool=True,random_rotation:bool=True) -> None:
+        """sumary_line
+        
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        
+        self.color_jitter=color_jitter
+        self.random_grayscale=random_grayscale
+        self.random_horizontal_flip = random_horizontal_flip
+        self.random_rotation=random_rotation
+        self.random_vertical_flip=random_vertical_flip
+        self.compose_list = []
+    
+    def color_jitter_pp(self):
+        """sumary_line
+        
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        self.compose_list.append(ColorJitter(0.125,0.125,0.125,0.125))
+        
+    def random_grayscale_pp(self):
+        """sumary_line
+        
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        self.compose_list.append(RandomGrayscale())
+        
+    def random_horizontal_flip_pp(self):
+        """sumary_line
+        
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        self.compose_list.append(RandomHorizontalFlip())
+        
+    def random_rotation_pp(self):
+        """sumary_line
+        
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        self.compose_list.append(RandomRotation(180))
+        
+    def random_vertical_flip_pp(self):
+        """sumary_line
+        
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        self.compose_list.append(RandomVerticalFlip())
+
+        
+    def forward(self):
+        """sumary_line
+        
+        Keyword arguments:
+        argument -- description
+        Return: return_description
+        """
+        if self.color_jitter:
+            self.color_jitter_pp()
+        if self.random_grayscale:
+            self.random_grayscale_pp()
+        if self.random_horizontal_flip:
+            self.random_horizontal_flip_pp()
+        if self.random_rotation:
+            self.random_rotation_pp()
+        if self.random_vertical_flip:
+            self.random_vertical_flip_pp()
+        transformation = Compose(self.compose_list)
+        return transformation
